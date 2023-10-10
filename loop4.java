38387a39fb2ede266fe6d84d1b23597205830045
sangeethaa-it21 <sangeethaa.it21@kascsathy.ac.in>
2023-10-10 05:32:00 +0000

    Loops

---
+++ loop4.java
@@ -0,0 +1,12 @@
+package loops;
+
+class Program4 {
+	public static void main(String args[]) {
+		int i=0;
+		while (i<=10) {
+			System.out.printf(" %d ", i);
+			i = i + 1;
+		}
+		System.out.printf("\n");
+	}
+}
